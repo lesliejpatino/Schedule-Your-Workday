10fade7cb7b7218f6240d9254236b7140db31b48
Leslie Patino <lesliejpatino@gmail.com>
2022-01-06 21:00:56 -0800

    added a function that will save the users input once they click the save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,17 @@
 var today = moment();
+var toDo = $('.to-do');
+var saveBtn = $('.saveBtn');
+
+// show the current date
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+for (i = 0; i < saveBtn.length; i++) {
+    $(saveBtn[i]).on("click", saveTask)   
+}
+
+function saveTask(e) {
+    var hourSelected = e.currentTarget.dataset.index;
+    var saveText = toDo[hourSelected].value;
+    localStorage.setItem('task', saveText);
+}
 
