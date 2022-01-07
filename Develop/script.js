205eacd313408070240e416ef001a1d639e08767
Leslie Patino <lesliejpatino@gmail.com>
2022-01-07 08:59:21 -0800

    this is temporarily replacing the text content with the last entered input upon refresh

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,7 @@ var saveBtn = $('.saveBtn');
 // show the current date
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+
 for (i = 0; i < saveBtn.length; i++) {
     $(saveBtn[i]).on("click", saveTask)   
 }
@@ -12,6 +13,13 @@ for (i = 0; i < saveBtn.length; i++) {
 function saveTask(e) {
     var hourSelected = e.currentTarget.dataset.index;
     var saveText = toDo[hourSelected].value;
+    console.log(saveText);
+
+    //save users input in local storage
     localStorage.setItem('task', saveText);
 }
+
+var getTask = localStorage.getItem('task');
+// this is temporarily replacing the inner html of all the textboxes with the last entered input
+$(toDo).html(getTask);
 
